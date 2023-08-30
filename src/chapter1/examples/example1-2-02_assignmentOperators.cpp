a135e2475b2df1b00e20144b2b93c795081c3d54
rrerusu <rrerusu@gmail.com>
2023-08-29 20:41:37 -0500

    Assignment Operators

---
+++ src/chapter1/examples/example1-2-02_assignmentOperators.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+    int         i = 10;
+    int         j = 5;
+    std::string s = "yea";
+
+    i -= 4;
+    j *= -2;
+    s += " or no";
+
+    return EXIT_SUCCESS;
+}
