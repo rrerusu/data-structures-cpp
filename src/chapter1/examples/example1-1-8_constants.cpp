ab6eff9b353e772de8bf645c638742719b94925a
rrerusu <rrerusu@gmail.com>
2023-08-29 20:10:26 -0500

    Constants and rename file names

---
+++ src/chapter1/examples/example1-1-8_constants.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+    const double PI         = 3.14159265;
+    const int    CUT_OFF[]  = {90, 80, 70, 60};
+    const int    N_DAYS     = 7;
+    const int    N_HOURS    = 24 * N_DAYS;
+    int counter[N_HOURS];
+
+    return EXIT_SUCCESS;
+}
