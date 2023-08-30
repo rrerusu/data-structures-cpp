f74c35cfbcdae8d56e230c715487ac77758ff765
rrerusu <rrerusu@gmail.com>
2023-08-29 20:12:28 -0500

    typedef

---
+++ src/chapter1/examples/example1-1-9_typedef.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+    typedef char* BufferPtr;
+    typedef double Coordinate;
+
+    BufferPtr p;
+    Coordinate x, y;
+
+    return EXIT_SUCCESS;
+}
