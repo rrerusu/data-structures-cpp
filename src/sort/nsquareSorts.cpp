dd90abfce7749ffeacce3f23408a4670b24253ee
rrerusu <rrerusu@gmail.com>
2023-11-14 16:02:44 -0600

    Modified bubblesort

---
+++ src/sort/nsquareSorts.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+template <typename T> void swap(T * val1, T * val2) {
+    T temp = * val1;
+    * val1 = * val2;
+    * val2 = temp;
+}
+
+void bubblesort(int arr[], int size) {
+    bool didSwap;
+    for(int currentVal = 0; currentVal < size; currentVal++) {
+        didSwap = false;
+        for(int checkVal = 0; checkVal < size - currentVal - 1; checkVal++) {
+            if(arr[checkVal] > arr[checkVal + 1]) {
+                swap<int>(&arr[checkVal], &arr[checkVal + 1]);
+                didSwap = true;
+            }
+        }
+        if(!didSwap)
+            break;
+    }
+}
+
+void printArr(int arr[], int size) {
+    for(int i = 0; i < size; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+}
+
+int main() {
+    int myArray[] = {1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17};
+
+    bubblesort(myArray, 11);
+
+    printArr(myArray, 11);
+
+    return EXIT_SUCCESS;
+}
